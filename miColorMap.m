a065008162bb6dfc45d118d0cb6b0ad5fc8236a7
Claudio <cladelpino@gmail.com>
2021-06-23 15:06:54 -0300

    Color map custom init II

--- miColorMap.m
+++
@@ -1,3 +0,0 @@
-function matrizC = miColorMap(concentracion,concMax)
-  matrizC = horzcat((concentracion/concMax),zeros(length(concentracion),1),1-(concentracion/concMax))
-endfunction
--- miPropioMapa.m
+++ miPropioMapa.m
@@ -1,3 +1,5 @@
 function salida = miPropioMapa(vector)
 % salida debe ser una matriz de kx3 donde k es el número de elementos en el argumento de entrada vector, con las componentes entre 0 y 1 de R G B en cada una de las columnas respectivas.
+% ej
+salida = horzcat(vector,zeros(length(vector),1),1-vector);
 end
