c4c5c7236aad94ad1d03a5c1df6555bf27de8708
Claudio <cladelpino@gmail.com>
2021-06-23 14:57:04 -0300

    Inicializar para mapa custom

---
+++ miPropioMapa.m
@@ -0,0 +1,3 @@
+function salida = miPropioMapa(vector)
+% salida debe ser una matriz de kx3 donde k es el número de elementos en el argumento de entrada vector, con las componentes entre 0 y 1 de R G B en cada una de las columnas respectivas.
+end
