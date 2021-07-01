73de5cf0eb937ed8f9a29764c5898a8849260ad7
cladelpino <cladelpino@gmail.com>
2021-07-01 20:54:11 -0300

    Primer Ejemplo init finish

--- generarMatrizUbicacionesNodos.m
+++ generarMatrizUbicacionesNodos.m
@@ -1,6 +1,14 @@
-function lista = generarMatrizUbicacionesNodos()
-       % Genera una linea de nodos ubicados a lo largo de la recta y = x
-       vectorFila = 1:5
-       vectorColumna = transpose(vectorFila);
-       lista = [vectorColumna , vectorColumna];
+function lista = generarMatrizUbicacionesNodos2()
+  % Genera una grilla de nodos
+  x=1:2;
+  y=1:3;
+  lista=zeros(length(x)*length(y),2);
+  k=1;
+  for i=1:length(x)
+    for j=1:length(y)
+      lista(k,1)=x(i);
+      lista(k,2)=y(j);
+      k=k+1;
+    endfor
+  endfor
 endfunction
